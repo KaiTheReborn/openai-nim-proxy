332779ea1d5f0b1701abd76f57594ec51eac3ac6
KaiTheReborn <124883245+KaiTheReborn@users.noreply.github.com>
2026-08-21 00:37:21 +0300

    Update server.js
    new modelz

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ const ENABLE_THINKING_MODE = false; // Set to true to enable chat_template_kwarg
 const MODEL_MAPPING = {
   'gpt-3.5-turbo': 'nvidia/llama-3.1-nemotron-ultra-253b-v1',
   'dsv3.9': 'deepseek-ai/deepseek-v4-pro',
-  'dsv4': 'deepseek-ai/deepseek-v4-flash',
+  'dsv4': 'deepseek-ai/deepseek-v4-flash-0731',
   'glms5': 'z-ai/glm-5.2',
-  'claude-3-opus': 'openai/gpt-oss-120b',
+  'qwen': 'qwen/Qwen3.8-2.4T-A95B',
   'claude-3-sonnet': 'openai/gpt-oss-20b',
   'gemini-pro': 'qwen/qwen3-next-80b-a3b-thinking' 
 };
